6af1829966b846f93c9995c350e2d7dbba2efdb3
aaccheca <aac.checa@yavirac.edu.ec>
2022-07-31 23:53:46 -0500

    Diseño

--- Frontend/lib/screens/home_screen.dart
+++ Frontend/lib/screens/home_screen.dart
@@ -10,11 +10,41 @@ class HomeScreen extends StatelessWidget {
     final menuOptions = AppRoutes.menuOptions;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Página Principal')
+        centerTitle: true,
+        title: Stack(
+        children: <Widget>[
+          /*const Icon(
+            Icons.apps_outlined,
+            color: Color.fromARGB(255, 216, 250, 27),
+            size: 40.0
+          ),*/
+    // Stroked text as border.
+        Text(
+        'Pagina Principal',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+        fontSize: 40,
+        foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 6
+          ..color = Color.fromARGB(255, 39, 37, 37),  
+    ),
+    ),
+    // Solid text as fill.
+    const Text(
+      'Pagina Principal',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 40,
+        color: Color.fromARGB(255, 255, 255, 255),
+      ),
+    ),
+  ],
+)
       ),
       body: ListView.separated(
           itemBuilder: (context, index) => ListTile(
-                leading: Icon(menuOptions[index].icon,color: AppTheme.primary),
+                leading: Icon(menuOptions[index].icon,color: Color.fromARGB(255, 216, 250, 27)),
                 title: Text(menuOptions[index].name),
                 onTap: () {
                   Navigator.pushNamed(context, menuOptions[index].route);
